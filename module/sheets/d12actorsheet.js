ae7c2aa0694807f73c4dacbec7727153a051b6d2
Natsil5 <livernoistristan@gmail.com>
2023-01-30 19:38:37 +0100

    1

--- module/sheets/d12actorsheet.js
+++ module/sheets/d12actorsheet.js
@@ -181,7 +181,9 @@ _prepareCharacterItems(sheetData) {
       
 
       let r = new Roll(jetdeDesFormule);
+      console.log("1")
       var roll=r.evaluate({"async": false});
+      console.log("2")
       
       if(valeur=="armes"){
           var total=r.total;
